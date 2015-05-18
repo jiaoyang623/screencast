897e0990fa8506ee15c83874441c1d5363da230a
jiaoyang623 <yangjiao623@gmail.com>
2015-05-18 14:21:24 +0800

    new file:   airplay/Bravo/AndroidManifest.xml 	new file:   airplay/Bravo/ant.properties 	new file:   airplay/Bravo/build.xml 	new file:   airplay/Bravo/local.properties 	new file:   airplay/Bravo/proguard-project.txt 	new file:   airplay/Bravo/project.properties 	new file:   airplay/Bravo/res/drawable-hdpi/ic_launcher.png 	new file:   airplay/Bravo/res/drawable-ldpi/ic_launcher.png 	new file:   airplay/Bravo/res/drawable-mdpi/ic_launcher.png 	new file:   airplay/Bravo/res/drawable-xhdpi/ic_launcher.png 	new file:   airplay/Bravo/res/layout/main.xml 	new file:   airplay/Bravo/res/values/strings.xml 	new file:   airplay/Bravo/src/org/daniel/android/airplay/HomeActivity.java

---
+++ airplay/Bravo/src/org/daniel/android/airplay/HomeActivity.java
@@ -0,0 +1,15 @@
+package org.daniel.android.airplay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
