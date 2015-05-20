2297e5ed2c2cfa63128d24496c293a61f2cb46e6
jiaoyang623 <yangjiao623@gmail.com>
2015-05-20 17:24:58 +0800

    new file:   miracast/MiraTest/AndroidManifest.xml 	new file:   miracast/MiraTest/ant.properties 	new file:   miracast/MiraTest/build.xml 	new file:   miracast/MiraTest/local.properties 	new file:   miracast/MiraTest/proguard-project.txt 	new file:   miracast/MiraTest/project.properties 	new file:   miracast/MiraTest/src/org/daniel/android/miracast/MainActivityTest.java 	modified:   .gitignore 	modified:   miracast/MiraTango/src/org/daniel/android/miracast/MainActivity.java

---
+++ miracast/MiraTest/src/org/daniel/android/miracast/MainActivityTest.java
@@ -0,0 +1,21 @@
+package org.daniel.android.miracast;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class org.daniel.android.miracast.MainActivityTest \
+ * org.daniel.android.miracast.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("org.daniel.android.miracast", MainActivity.class);
+    }
+
+}
